8b7dfc1fa5c88be67049a1f6c7067b5e4a719ca2
saurabhla <69839666+saurabhla@users.noreply.github.com>
2025-10-27 20:01:38 +0000

    Create scripts01_change_over_time_analysis.sql

---
+++ scripts01_change_over_time_analysis.sql
@@ -0,0 +1,32 @@
+SELECT 
+  YEAR(order_date) as YEAR,
+  SUM(sales_amount) as total_sales,
+  COUNT(DISTINCT customer_key) as total_customers,
+  SUM(quantity) as total_quantity
+FROM
+  gold.fact_sales
+  WHERE order_date IS NOT NULL
+  GROUP BY YEAR(order_date)
+  ORDER BY YEAR(order_date);
+
+SELECT 
+  DATETRUNC(month,order_date) as order_date,
+  SUM(sales_amount) as total_sales,
+  COUNT(DISTINCT customer_key) as total_customers,
+  SUM(quantity) as total_quantity
+FROM
+  gold.fact_sales
+  WHERE order_date IS NOT NULL
+  GROUP BY DATETRUNC(month,order_date)
+  ORDER BY DATETRUNC(month,order_date);
+
+SELECT 
+  FORMAT(order_date,'yyyy-MMM') as order_date,
+  SUM(sales_amount) as total_sales,
+  COUNT(DISTINCT customer_key) as total_customers,
+  SUM(quantity) as total_quantity
+FROM
+  gold.fact_sales
+  WHERE order_date IS NOT NULL
+  GROUP BY FORMAT(order_date,'yyyy-MMM')
+ORDER BY FORMAT(order_date,'yyyy-MMM');
